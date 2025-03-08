b7f1b02884a704f2ae1afbb46763ced0a33862ea
GC; Pins <spot.meow.meow@gmail.com>
2025-03-08 01:54:29 -0500

    dang it do not be working the way it supposed to be working do it be

--- js/background.js
+++ js/background.js
@@ -4,7 +4,6 @@ chrome.runtime.onInstalled.addListener(async () => {
         title: "Describe",
         type: "normal",
         contexts: ["image"]
-        //contexts: ["all"],
     });
 });
 
@@ -12,6 +11,7 @@ chrome.contextMenus.onClicked.addListener(async (callback) => {
     console.log("IMG URL: " + callback.srcUrl);
 });
 
-chrome.commands.onCommand.addListener((cmd, tab) => {
-    console.log(JSON.stringify(cmd));
+chrome.commands.onCommand.addListener((command, tab) => {
+    console.log(JSON.stringify(command));
+    console.log(JSON.stringify(tab));
 });
